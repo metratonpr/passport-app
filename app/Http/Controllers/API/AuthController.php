e7a23913de7444557645ef3f268c84f4c40b4307
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-10-23 21:11:05 -0300

    AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,14 +2,40 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    function login(Request $request)
+    {
+        //pegar o usuario e senha
+        $credentials = $request->validate([
+            'email' => 'string|required|email',
+            'password' => 'string|required'
+        ]);
+        //verificar se ele existe no banco de dados
+        //Auth::attempt($credentials)
+        if (auth()->attempt($credentials)) {
+            $user = Auth::user();
+            $user['token'] = $user->createToken('Passaport App')->accessToken;
+            return response()->json([
+                'user' => $user
+            ], 200);
+        }
+        // em caso de erro
+        return response()->json([
+            'message' => 'Credenciais estão invalidas'
+        ], 402);
     }
 
 }
