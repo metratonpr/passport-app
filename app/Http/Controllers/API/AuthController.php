4501d42f35556e0f25b1025ffccd422c7f977d0d
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-10-25 20:08:00 -0300

    Ajuste AuthController

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 
 class AuthController extends Controller
@@ -16,53 +17,74 @@ class AuthController extends Controller
         $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
-    function login(Request $request)
-    {
-        //pegar o usuario e senha
-        $credentials = $request->validate([
-            'email' => 'string|required|email',
-            'password' => 'string|required'
-        ]);
-        //verificar se ele existe no banco de dados
-        //Auth::attempt($credentials)
-        if (auth()->attempt($credentials)) {
-            $user = Auth::user();
-            $user['token'] = $user->createToken('Passaport App')->accessToken;
-            return response()->json([
-                'user' => $user
-            ], 200);
-        }
-        // em caso de erro
+    public function login(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+    ]);
+
+    if ($validator->fails()) {
         return response()->json([
-            'message' => 'Credenciais estão invalidas'
-        ], 402);
+            'message' => 'Dados de entrada inválidos',
+            'errors' => $validator->errors(),
+        ], 422); // Código 422 para Entidade não processável (Unprocessable Entity)
     }
 
-    function logout()
-    {
-        Auth::user()->tokens()->delete();
+    if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        $user = Auth::user();
+        $token = $user->createToken('Passport App')->accessToken;
+
         return response()->json([
-            'message' => 'Logout realizado com sucesso!',
-        ]);
+            'user' => $user,
+            'token' => $token,
+        ], 200);
     }
 
-    function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 
-            'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+    return response()->json([
+        'message' => 'Credenciais inválidas'
+    ], 401); // Código 401 para Não Autorizado (Unauthorized)
+}
+
+public function logout()
+{
+    try {
+        Auth::guard('api')->user()->token()->revoke();
+    } catch (\Exception $e) {
         return response()->json([
-            'message' => 'Usuario criado com sucesso',
-            'user' => $user
-        ]);
+            'message' => 'Token inválido ou não autorizado.',
+        ], 401);
     }
 
+    return response()->json([
+        'message' => 'Logout realizado com sucesso!',
+    ], 200);
+}
+
+    public function register(Request $request)
+{
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6',
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Erro de validação',
+            'errors' => $validator->errors()
+        ], 422); // Código 422 para entidades não processáveis
+    }
+
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+
+    return response()->json([
+        'message' => 'Usuário criado com sucesso',
+        'user' => $user
+    ], 201); // Código 201 para criação bem-sucedida
+}
 }
